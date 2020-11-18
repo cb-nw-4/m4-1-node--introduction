66f091abdd0e40a05da136d354d6101919b4567b
chris shino <chris.shino@hotmail.com>
2020-11-18 14:01:14 -0500

    done #3

--- public/parrot/scripts.js
+++ public/parrot/scripts.js
@@ -25,11 +25,11 @@ const sendMessage = (event) => {
 
   const message = { author: 'user', text: messageInput.value };
   updateConversation(message);
+  const messageUrl = `/parrot-message?text=${message.text}`
 
-  fetch('/parrot-message')
+  fetch(messageUrl)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       updateConversation(data.message);
     });
 };
--- server.js
+++ server.js
@@ -46,6 +46,15 @@ express()
       res.status(200).json({ status: 200, message });
     }, randomTime);
   })
+
+  .get("/parrot-message", (req, res) => {
+    const message = req.query;
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+
+  })
   // add new endpoints here ☝️
   // ---------------------------------
   // Nothing to modify below this line
