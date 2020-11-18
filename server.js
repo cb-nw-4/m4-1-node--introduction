c4c73fdb5114de65453294fc1dda0576d8e1c9ee
ferielnabet <fnabet@ipconnex.com>
2020-11-17 20:57:08 -0500

    exercise 2 done

--- server.js
+++ server.js
@@ -27,6 +27,27 @@ express()
     }, randomTime);
   })
 
+  .get('/monkey-message', (req, res) => {
+    const messages = [
+      "Don’t monkey around with me.",
+      "If you pay peanuts, you get monkeys.",
+      "I fling 💩 at you!",
+      "🙊",
+      "🙈",
+      "🙉",
+    ];
+
+    const message= {author:'monkey', text: messages[Math.floor(Math.random() * messages.length)]}
+
+    const randomTime= Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({status: 200, message });
+
+    }, randomTime);
+  })
+
+
+
   // add new endpoints here ☝️
   // ---------------------------------
   // Nothing to modify below this line
