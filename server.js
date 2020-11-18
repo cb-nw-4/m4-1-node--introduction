3003beefa854c9eb5d70de6b1c8cca393508ee85
Sandra Bessette <sandrabess@hotmail.com>
2020-11-18 15:18:26 -0500

    exercise 3 completed

--- server.js
+++ server.js
@@ -53,6 +53,14 @@ express()
     }, randomTime);
   })
 
+  .get('/parrot-message', (req, res) => {    
+    const message =  { author: 'parrot', text: req.query.text };   
+    const randomTime = Math.floor(Math.random() * 3000);   
+    setTimeout(() => {
+          res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
+
   // this is our catch all endpoint. If a user navigates to any endpoint that is not
   // defined above, they get to see our 404 page.
   .get('*', (req, res) => {
