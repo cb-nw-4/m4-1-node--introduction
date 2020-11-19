51186b7ecaabe508c740209605322de12cacbaab
Doug Wright <dw@nof49.ca>
2020-11-19 12:23:44 -0500

    Complete exercise 2

--- server.js
+++ server.js
@@ -17,13 +17,34 @@ express()
   // Nothing to modify above this line
   // ---------------------------------
   // add new endpoints here 👇
-.get('/cat-message', (req, res) => {
-  const message = { author: 'cat', text: 'Meow'};
-  const randomTime = Math.floor(Math.random() * 3000);
-  setTimeout(() => {
-    res.status(200).json({status: 200, message});
-  }, randomTime);
+
+  // Cat enpoint
+  .get('/cat-message', (req, res) => {
+    const message = { author: 'cat', text: 'Meow'};
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({status: 200, message});
+    }, randomTime);
   })
+
+  // Monkey endpoint
+  .get('/monkey-message', (req, res) => {
+    const messages = [
+      "Don’t monkey around with me.",
+      "If you pay peanuts, you get monkeys.",
+      "I fling 💩 at you!",
+      "🙊",
+      "🙈",
+      "🙉",
+    ];
+    const randomMsg = Math.floor(Math.random() * 6);
+    const message = { author: 'monkey', text: messages[randomMsg]};
+    const randomTime = Math.floor(Math.random() * 3000);
+
+    setTimeout(() => {
+      res.status(200).json({status: 200, message});
+    }, randomTime);
+  })  
   // add new endpoints here ☝️
   // ---------------------------------
   // Nothing to modify below this line
