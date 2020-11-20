f3c5b5573a86984919b41aa993088c82af9786ce
Vanessa Chan <chw.vanessa@gmail.com>
2020-11-20 15:35:00 -0500

    Ex 5

--- server.js
+++ server.js
@@ -91,17 +91,20 @@ express()
 
   // Node spins up our server and sets it to listen on port 8000.
   .listen(8000, () => console.log(`Listening on port 8000`));
-
+  
+  
+  let isJoke = false;
   const getBotMessage = (text) => {
     const commonGreetings = ["hi", "hello", "howdy","hey"];
     const commonGoodbyes = ["bye", "goodbye", "see you","ciao"];
+    const jokePrompt = "something funny";
 
     let botMsg = "Bzzt ";
     let isGreeting = false;
     let isGoodbye = false;
 
     commonGreetings.map((greeting)=>{
-      if(text.toLowerCase().includes(greeting)){
+      if(text.toLowerCase().startsWith(greeting)){
         isGreeting = true;
       }
     });
@@ -112,13 +115,42 @@ express()
       }
     });
 
+    if(text.toLowerCase().includes(jokePrompt)){
+        isJoke=true;
+    }
+
     if (isGreeting) {
       botMsg += "Hello.";
     }else if(isGoodbye){
       botMsg += "Bye.";
+    }else if(isJoke){
+      botMsg="Do you want to hear a joke? Reply YES or NO";
+
+      if(text.toLowerCase().includes("yes")){
+        botMsg=getJoke();
+        isJoke=false;
+      }else if(text.toLowerCase().includes("no")){
+        botMsg="goodbye...";
+        isJoke=false;
+      }
+
     }
     else{
       botMsg += `"${text}"`;
     }
     return botMsg;
+  };
+
+  const getJoke = ()=>{
+    const jokes = [
+
+      "What do you call an aligator in a vest? An investigator.",
+    "Becoming a vegetarian is a big missed steak.",
+    "Why did the banada go to the doctor? It wasn't peeling well.",
+    "A user interface is like a joke. If you have to explain it, it's not that good.",
+    "How many programmers does it take to change a light bulb? None – It’s a hardware problem."
+    ];
+
+    return jokes[Math.floor(Math.random()*5)];
+
   };
