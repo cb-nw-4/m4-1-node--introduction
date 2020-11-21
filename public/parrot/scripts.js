2e33237442464ecf15f67b10782b41e2185deab2
“Rebecca <Rebecca.gilhespy@gmail.com>
2020-11-21 15:47:07 -0500

    WS4React

--- public/parrot/scripts.js
+++ public/parrot/scripts.js
@@ -1,37 +1,38 @@
-const messageInput = document.querySelector('#user-input');
-const conversationElem = document.querySelector('#conversation-container');
+const messageInput = document.querySelector("#user-input");
+const conversationElem = document.querySelector("#conversation-container");
 
 // focus the input on load
 const handleFocus = () => {
-  messageInput.focus();
+    messageInput.focus();
 };
 
 // updateConversation expects an object with 'user' and 'text'
 const updateConversation = (message) => {
-  const { author, text } = message;
-  const messageElem = document.createElement('p');
+    const { author, text } = message;
+    const messageElem = document.createElement("p");
 
-  messageElem.classList.add('message', author);
-  messageElem.innerHTML = `<span>${text}</span>`;
-  conversationElem.appendChild(messageElem);
-  conversationElem.scrollTop = conversationElem.scrollHeight;
+    messageElem.classList.add("message", author);
+    messageElem.innerHTML = `<span>${text}</span>`;
+    conversationElem.appendChild(messageElem);
+    conversationElem.scrollTop = conversationElem.scrollHeight;
 
-  if (author === 'user') messageInput.value = '';
-  handleFocus();
+    if (author === "user") messageInput.value = "";
+    handleFocus();
 };
 
 const sendMessage = (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const message = { author: 'user', text: messageInput.value };
-  updateConversation(message);
+    const text = messageInput.value;
+    const message = { author: "user", text: messageInput.value };
 
-  fetch('/parrot-message')
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      updateConversation(data.message);
-    });
+    updateConversation(message);
+    fetch(`/parrot-message/?text=${text}`)
+        .then((res) => res.json())
+        .then((data) => {
+            updateConversation(data.message);
+            console.log(data);
+        });
 };
 
 // call handleFocus on load
