effe7cba6ddcb2aee8aa1a0f023facff72707f2b
sfillionraff <sfillionraff@gmail.com>
2020-11-21 11:38:39 -0500

    to exercise 4

--- public/cat/scripts.js
+++ public/cat/scripts.js
@@ -1,1 +1,36 @@
-// here there be JS, yarrr ☠️
+const messageInput = document.querySelector("#user-input");
+const conversationElement = document.querySelector("#conversation-container");
+
+const handleFocus = () => {
+    messageInput.focus();
+}
+
+const updateConversation = (message) => {
+    const { author, text } = message;
+    const messageElement = document.createElement("p");
+    messageElement.classList.add("message", author);
+    messageElement.innerHTML = `<span>${text}</span`;
+    conversationElement.appendChild(messageElement);
+    conversationElement.scrollTop = conversationElement.scrollHeight;
+    if (author === "user") {
+        messageInput.value = "";
+    }
+    handleFocus();
+    console.log(message);
+}
+
+const sendMessage = (event) => {
+    // prevent the default "page reload" from occurring.
+    event.preventDefault();
+    const message = { author: "user", text: messageInput.value };
+    updateConversation(message);
+    // GET call to the cat-message endpoint
+    fetch("/cat-message")
+        .then((res) => res.json())
+        .then((data) => {
+            updateConversation(data.message);
+    });
+    // receive this error message: Uncaught (in promise) SyntaxError: JSON.parse: unexpected character at line 1 column 1 of the JSON data
+}
+
+handleFocus();
