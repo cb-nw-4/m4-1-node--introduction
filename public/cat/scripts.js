3e703c073cb903f7192c4bf8570a5e27e60e8f61
Lara Tunc <laratunc@gmail.com>
2020-11-20 20:32:03 -0500

    done

--- public/cat/scripts.js
+++ public/cat/scripts.js
@@ -1,1 +1,37 @@
 // here there be JS, yarrr ☠️
+
+const messageInput = document.querySelector("#user-input");
+const conversationElem = document.querySelector("#conversation-container");
+
+const handleFocus = () => {
+    messageInput.focus();
+};
+
+const sendMessage = (event) => {
+    // prevent the default "page reload" from occurring.
+    event.preventDefault();
+    const message = { author: "user", text: messageInput.value };
+    updateConversation(message);
+    fetch("/cat-message")
+        .then((res) => res.json())
+        .then((data) => {
+        //console.log(data);
+        updateConversation(data.message);
+    });
+};
+
+const updateConversation = (message) => {
+    // console.log(message);
+    const { author, text } = message;
+    const messageElem = document.createElement("p");
+    messageElem.classList.add("message", author);
+    messageElem.innerHTML = `<span>${text}</span>`;
+    conversationElem.appendChild(messageElem);
+    if (author === "user") {
+        messageInput.value = "";
+    }; 
+    handleFocus();
+    conversationElem.scrollTop = conversationElem.scrollHeight;
+};
+
+handleFocus();
--- server.js
+++ server.js
@@ -4,6 +4,29 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const messages = [
+  "Don’t monkey around with me.",
+  "If you pay peanuts, you get monkeys.",
+  "I fling 💩 at you!",
+  "🙊",
+  "🙈",
+  "🙉",
+  "What is a monkey’s favourite dance move? The banana split.",
+  "I like to move it move it",
+  "Who's a monkey's favourite Hogwarts teacher? Professor SnAPE!",
+  "How do you open a banana? With a monKEY!",
+  "What sort of monkeys feel unwell? Gor-ILL-as!",
+  "🍌",
+  "🍌🍌",
+  "🍌🍌🍌",
+  "🐒",
+  "🐵",
+  "🌴",
+  "🥥"
+];
+
+let somethingFunny = false; 
+
 express()
   // Below are methods that are included in express(). We chain them for convenience.
   // --------------------------------------------------------------------------------
@@ -17,6 +40,78 @@ express()
   // Nothing to modify above this line
   // ---------------------------------
   // add new endpoints here 👇
+
+  .get('/cat-message', (req, res) => {
+    const message = { author: 'cat', text: 'Meow' };
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
+
+  .get('/monkey-message', (req, res) => {
+    const randomMessageIndex = Math.floor(Math.random() * (messages.length-1));
+    let randomMessage = messages[randomMessageIndex];
+    const message = { author: 'monkey', text: randomMessage };
+    const randomTime = Math.floor(Math.random() * 3000); 
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
+
+  .get('/parrot-message', (req, res) => {
+    const message = { author: 'parrot', text: req.query.message };
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+    console.log(req.query);
+  })
+
+  .get('/bot-message', (req, res) => {
+    const getBotMessage = (text) => {
+      let botMsg; 
+      const commonGreetings = ["hi", "hello", "howdy"];
+      const commonGoodbyes = ["goodbye","bye","bye bye","xoxo","peace out","toodles"];
+      botMsg = 'Bzzt "'+text+'"';
+      commonGreetings.forEach ((item)=> {
+        if (text.toLowerCase().includes(item)) {
+          botMsg = "Bzzt Hello!";
+        };
+      });
+      commonGoodbyes.forEach ((item)=> {
+        if (text.toLowerCase().includes(item)) {
+          botMsg = "Bzzt Take care!";
+        };
+      });
+      const jokes = ["How do you get a squirrel to like you?\n Act like a nut.",
+      "Why don't eggs tell jokes?\n They'd crack each other up.",
+      "I don't trust stairs. They're always up to something.",
+      "What do you call someone with no body and no nose?\n Nobody knows.",
+      "Did you hear the rumor about butter?\n Well, I'm not going to spread it!",
+      "Why couldn't the bicycle stand up by itself?\n It was two tired.",
+      "What do you call a fake noodle?\n An impasta."
+      ]; 
+      if(text.toLowerCase().includes("something funny")) {
+        botMsg = "Do you want to hear a joke? Yes/No";
+        somethingFunny = true; 
+      };
+      if(text.toLowerCase().includes("yes") && somethingFunny) {
+        botMsg = `${jokes[Math.round(Math.random()*(jokes.length-1))]} \n For another one reply: Yes/No`;
+      };
+      if(text.toLowerCase().includes("no") && somethingFunny ) {
+        botMsg = "Bzzt Goodbye";
+        somethingFunny = false; 
+      };
+      return botMsg; 
+    };
+    const message = { author: 'bot', text: getBotMessage(req.query.message) };
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+    console.log(req.query);
+  })
 
   // add new endpoints here ☝️
   // ---------------------------------
